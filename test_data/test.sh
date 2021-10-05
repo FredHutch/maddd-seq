a9b874011aa3a27d7028431471720ee4884c6085
Sam Minot <sminot@fredhutch.org>
2021-10-05 10:57:41 -0700

    Run tests on different manifest files

--- test_data/test.sh
+++ test_data/test.sh
@@ -2,11 +2,24 @@
 
 set -Eeuo pipefail
 
-nextflow \
-    run \
-    -c ../nextflow.config \
-    ../main.nf \
-    -profile docker \
-    --sample_sheet manifest.csv \
-    --output output \
-    -resume
+# The user must specimen a manifest
+MANIFEST=$1
+
+if (( ${#MANIFEST} == 0 )); then
+
+    echo "Please specify manifest"
+
+else
+
+    echo "Running data in manifest: $MANIFEST"
+
+    nextflow \
+        run \
+        -c ../nextflow.config \
+        ../main.nf \
+        -profile docker \
+        --sample_sheet $MANIFEST \
+        --output ${MANIFEST%.csv}.output \
+        -resume
+
+fi
